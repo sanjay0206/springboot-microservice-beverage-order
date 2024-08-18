d06ef943b3916a76a16d7fcf434c4eddbf468663
sanjay0206 <62499148+sanjay0206@users.noreply.github.com>
2024-08-18 12:12:59 +0530

    new commit

--- auth-service/src/main/java/com/infybuzz/controller/AuthController.java
+++ auth-service/src/main/java/com/infybuzz/controller/AuthController.java
@@ -3,30 +3,23 @@ package com.infybuzz.controller;
 import com.infybuzz.entity.UserCredEntity;
 import com.infybuzz.repository.UserCredRepository;
 import com.infybuzz.request.AuthRequest;
+import com.infybuzz.request.RefreshTokenRequest;
 import com.infybuzz.security.CustomUserDetailsService;
+import com.infybuzz.service.TokenService;
+import com.infybuzz.utils.AuthUtils;
 import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JOSEObjectType;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.KeyPair;
-import java.time.Instant;
+import java.text.ParseException;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -35,94 +28,69 @@ import java.util.Map;
 public class AuthController {
     Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-    @Value("${spring.security.oauth2.authorization-server.jwt.issuer-uri}")
-    private String jwtIssuerUri;
-
-    @Autowired
-    private KeyPair keyPair;
-
-    @Autowired
-    private JWKSet jwkSet;
-
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
     @Autowired
-    UserCredRepository repository;
+    private UserCredRepository repository;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @GetMapping("/.well-known/jwks.json")
+    public Map<String, Object> getJwkSet() {
+        logger.info("Inside getJwkSet");
+        return tokenService.getJwkSet();
+    }
 
     @PostMapping("/register")
-    public UserCredEntity addNewUser(@RequestBody UserCredEntity user) {
-        logger.info("Inside addNewUser: " + user);
+    public UserCredEntity registerUser(@RequestBody UserCredEntity user) {
+        logger.info("Registering new user: " + user);
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setCreatedAt(LocalDateTime.now());
 
         return repository.save(user);
     }
 
-    @GetMapping("/.well-known/jwks.json")
-    public Map<String, Object> getJwkSet() {
-        logger.info("Inside getJwkSet");
-
-        return jwkSet.toJSONObject();
-    }
-
     @PostMapping("/token")
-    public Map<String, String> getToken(@RequestBody AuthRequest authRequest) throws JOSEException {
-        logger.info("Inside getToken = " + authRequest);
+    public Map<String, String> generateTokens(@RequestBody AuthRequest authRequest) throws JOSEException {
+        logger.info("Generating tokens for: " + authRequest.getUsername());
 
         UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
-        logger.info("userDetails = " + userDetails);
+        logger.info("User details loaded: " + userDetails);
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 authRequest.getUsername(),
                 authRequest.getPassword(),
                 userDetails.getAuthorities());
-        logger.info("authenticationToken = " + authenticationToken);
 
-        if (authenticationToken.isAuthenticated()) {
-            return generateJwtToken(authenticationToken);
+        if (!authenticationToken.isAuthenticated()) {
+            throw new RuntimeException("Authentication failed");
         }
 
-      return Map.of();
+        List<SimpleGrantedAuthority> authorities = AuthUtils.convertToSimpleGrantedAuthorities(userDetails.getAuthorities());
+
+        return tokenService.generateAccessAndRefreshTokens(authRequest.getUsername(), authorities);
     }
 
-    private Map<String, String> generateJwtToken(UsernamePasswordAuthenticationToken authenticationToken)
-            throws JOSEException {
+    @PostMapping("/refresh-token")
+    public Map<String, String> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) throws JOSEException, ParseException {
+        logger.info("Refreshing token");
 
-        // Extract scopes
-        List<String> roles = authenticationToken.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .map(authority -> authority.replace("ROLE_", ""))
-                .toList();
-        logger.info("roles list: " + roles);
+        // Parse the refresh token
+        SignedJWT signedJWT = SignedJWT.parse(refreshTokenRequest.getRefreshToken());
 
-        // Define JWT Claims
-        JWTClaimsSet claims = new JWTClaimsSet.Builder()
-                .subject(authenticationToken.getName())
-                .notBeforeTime(Date.from(Instant.now()))
-                .expirationTime(Date.from(Instant.now().plusSeconds(1800))) // 30 minutes expiry
-                .issueTime(Date.from(Instant.now()))
-                .issuer(jwtIssuerUri)
-                .claim("roles", roles)
-                .build();
+        // Extract username and validate refresh token
+        String username = signedJWT.getJWTClaimsSet().getSubject();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-        // Create JWS header with the key ID
-        JWK jwk = jwkSet.getKeys().get(0);
-        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256)
-                .keyID(jwk.getKeyID())
-                .type(JOSEObjectType.JWT)
-                .build();
+        List<SimpleGrantedAuthority> authorities = AuthUtils.convertToSimpleGrantedAuthorities(userDetails.getAuthorities());
 
-        // Create the signed JWT
-        SignedJWT signedJWT = new SignedJWT(header, claims);
-        RSASSASigner signer = new RSASSASigner(keyPair.getPrivate());
-        signedJWT.sign(signer);
-
-        // Serialize the JWT to a compact form
-        return Map.of("accessToken", signedJWT.serialize());
+        return tokenService.generateNewRefreshToken(signedJWT, authorities);
     }
 
 }
--- auth-service/src/main/java/com/infybuzz/security/CustomUserDetails.java
+++ auth-service/src/main/java/com/infybuzz/security/CustomUserDetails.java
@@ -1,6 +1,7 @@
 package com.infybuzz.security;
 
 import com.infybuzz.entity.UserCredEntity;
+import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@ToString
 public class CustomUserDetails implements UserDetails {
     private final String username;
     private final String password;
